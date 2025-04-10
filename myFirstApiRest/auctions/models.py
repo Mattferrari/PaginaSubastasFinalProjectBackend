3a8481b98786175f09f0757ebff8ee62ef4d2088
Mattferrari <202215419@alu.comillas.edu>
2025-04-10 23:34:05 +0200

    tutorial users hasta la 11

--- myFirstApiRest/auctions/models.py
+++ myFirstApiRest/auctions/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.contrib.auth.models import User
+from django.conf import settings
 
 
 # Create your models here.
@@ -38,12 +38,12 @@ class Auction(models.Model):
     
 class Bid(models.Model):
     auction = models.ForeignKey(Auction, related_name='bids', on_delete=models.CASCADE)
-    bidder = models.ForeignKey(User, related_name='bids', on_delete=models.CASCADE)  # Usuario que puja
-    amount = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
+    bidder = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
     bid_time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-bid_time'] 
 
     def __str__(self):
-        return f"{self.bidder.username} - {self.amount}€ en {self.auction.title}"
+        return f"{self.bidder} - {self.price}€ en {self.auction.title}"
